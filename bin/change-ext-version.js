7176c43993dca69d7a28adb2d27fa522bb7f579a
FutureDuck <iwonder@duckduckgo.com>
2020-06-11 16:17:54 -0700

    fix: npm and ext json version bumps

---
+++ bin/change-ext-version.js
@@ -0,0 +1,25 @@
+import fs from 'fs';
+
+const MANITFEST_FILE = 'src/manifest.json';
+const newVersion = process.argv[2];
+
+function exitWithError(error) {
+  console.log(error);
+  process.exit(2);
+}
+
+fs.readFile(MANITFEST_FILE, 'utf8', (err, fileContent) => {
+  if (err) {
+    exitWithError(err);
+  }
+  const newFileContent = fileContent.replace(
+    /"version": "\d+.\d+.\d+"/g,
+    `"version": "${newVersion}"`
+  );
+
+  fs.writeFile(MANITFEST_FILE, newFileContent, 'utf8', (err) => {
+    if (err) {
+      exitWithError(err);
+    }
+  });
+});
