5d3aee6f444e23dbc220dc2fa8c1625bd6900825
FutureDuck <iwonder@duckduckgo.com>
2020-06-13 16:06:44 -0700

    refactor: url fixtures export style

--- __tests__/shared/__url_fixtures.js
+++ __tests__/shared/__url_fixtures.js
@@ -5,16 +5,29 @@ const TEST_PAGE_PATH = `${HTML_PAGES_PATH}/test_page.html`;
 const GOOD_PAGE_PATH = `${HTML_PAGES_PATH}/good_page.html`;
 const BAD_PAGE_PATH = `${HTML_PAGES_PATH}/bad_page.html`;
 
-export const A_URL = 'https://example.com';
-export const GOOD_URL = 'https://duckduckgo.com';
-export const BAD_URL = 'https://evil-tracker.com';
-export const ANOTHER_URL = GOOD_URL;
-export const SOME_URLS = [A_URL, ANOTHER_URL, BAD_URL];
-export const LOCAL_TEST_PAGE_URL = getUrlFromFilePath(TEST_PAGE_PATH);
-export const LOCAL_GOOD_PAGE_URL = getUrlFromFilePath(GOOD_PAGE_PATH);
-export const LOCAL_BAD_PAGE_URL = getUrlFromFilePath(BAD_PAGE_PATH);
-export const BAD_URLS = [BAD_URL, LOCAL_BAD_PAGE_URL, 'https://facebook.com'];
-export const GOOD_URLS = [GOOD_URL, LOCAL_GOOD_PAGE_URL, 'https://eff.org'];
+const A_URL = 'https://example.com';
+const GOOD_URL = 'https://duckduckgo.com';
+const BAD_URL = 'https://evil-tracker.com';
+const ANOTHER_URL = GOOD_URL;
+const SOME_URLS = [A_URL, ANOTHER_URL, BAD_URL];
+const LOCAL_TEST_PAGE_URL = getUrlFromFilePath(TEST_PAGE_PATH);
+const LOCAL_GOOD_PAGE_URL = getUrlFromFilePath(GOOD_PAGE_PATH);
+const LOCAL_BAD_PAGE_URL = getUrlFromFilePath(BAD_PAGE_PATH);
+const BAD_URLS = [BAD_URL, LOCAL_BAD_PAGE_URL, 'https://facebook.com'];
+const GOOD_URLS = [GOOD_URL, LOCAL_GOOD_PAGE_URL, 'https://eff.org'];
+
+export {
+  A_URL,
+  GOOD_URL,
+  BAD_URL,
+  ANOTHER_URL,
+  SOME_URLS,
+  LOCAL_TEST_PAGE_URL,
+  LOCAL_GOOD_PAGE_URL,
+  LOCAL_BAD_PAGE_URL,
+  BAD_URLS,
+  GOOD_URLS,
+};
 
 function getUrlFromFilePath(relativePath) {
   const absolutePath = path.posix.resolve(relativePath);
