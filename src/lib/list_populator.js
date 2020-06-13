b4777da43041e858cb1b7c4d14dc9b0e167f9e31
FutureDuck <iwonder@duckduckgo.com>
2020-06-13 16:43:05 -0700

    refactor: list_populator

--- src/lib/list_populator.js
+++ src/lib/list_populator.js
@@ -1,17 +1,20 @@
 import List from './model/list.js';
-import { BAD_URLS } from '../../__tests__/shared/__url_fixtures.js';
+import { BAD_URLS } from '../shared/__url_fixtures.js';
+
+// TODO for now, just populating it with the examples from fixtures
+// TODO real deal: fetch from local files shipped with the extension, augment it with live sources...
+// https://github.com/lfilho/ddg-test-project/issues/24
+// https://github.com/lfilho/ddg-test-project/issues/18
+// https://github.com/lfilho/ddg-test-project/issues/17
 
 export default class ListPopulator {
-  static populateList(list) {
-    if (list.type === List.types.DENY_LIST) {
-      // TODO for now, just populating it with the examples from fixtures
-      // TODO real deal: fetch from local files shipped with the extension, augment it with live sources...
-      // https://github.com/lfilho/ddg-test-project/issues/24
-      // https://github.com/lfilho/ddg-test-project/issues/18
-      // https://github.com/lfilho/ddg-test-project/issues/17
-      list.bulkAdd(BAD_URLS);
-    } else if (list.type === List.types.ALLOW_LIST) {
-      //TODO https://github.com/lfilho/ddg-test-project/issues/8
-    }
+  static async populateList(list) {
+    const listTypeToUrlMapper = {
+      [List.types.DENY_LIST]: BAD_URLS,
+    };
+    const urls = listTypeToUrlMapper[list.type];
+    list.bulkAdd(urls);
+
+    //TODO https://github.com/lfilho/ddg-test-project/issues/8
   }
 }
