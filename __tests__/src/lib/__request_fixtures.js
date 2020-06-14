0df07bf1c8bb7783bfd94a51867a89689deebbd2
FutureDuck <iwonder@duckduckgo.com>
2020-06-13 23:21:37 -0700

    feat: emit metric when url is blocked
    Resolves #11

--- __tests__/src/lib/__request_fixtures.js
+++ __tests__/src/lib/__request_fixtures.js
@@ -1,7 +1,21 @@
-import { A_URL } from '../../../src/shared/__url_fixtures.js';
+import {
+  A_URL,
+  GOOD_URL,
+  BAD_URL,
+} from '../../../src/shared/__url_fixtures.js';
 
-export const RANDOM_REQUEST_DETAILS = { url: A_URL };
-export const BLOCKING_RESPONSE = {
+const RANDOM_REQUEST_DETAILS = { url: A_URL };
+const ALLOWED_REQUEST_DETAILS = { url: GOOD_URL };
+const DENIED_REQUEST_DETAILS = { url: BAD_URL };
+
+const BLOCKING_RESPONSE = {
   CANCELLED: { cancel: true },
   NOT_CANCELLED: { cancel: false },
+};
+
+export {
+  RANDOM_REQUEST_DETAILS,
+  ALLOWED_REQUEST_DETAILS,
+  DENIED_REQUEST_DETAILS,
+  BLOCKING_RESPONSE,
 };
