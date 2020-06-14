e5f8acf3f05b8fe967dcda2ece988d7240ff500c
FutureDuck <iwonder@duckduckgo.com>
2020-06-13 19:28:37 -0700

    feat: metric model

---
+++ __tests__/src/shared/model/metric.spec.js
@@ -0,0 +1,23 @@
+import Metric from '../../../../src/shared/model/metric.js';
+
+describe('Metric', () => {
+  it('should throw with an inexisting dimension', () => {
+    expect(() => {
+      new Metric('my-made-up-dimension', 1);
+    }).toThrow(/Invalid dimension/);
+    //TODO https://github.com/lfilho/ddg-test-project/issues/46
+  });
+
+  it('accepts a known dimension', () => {
+    expect(() => {
+      new Metric(Metric.dimensions.REQUEST_BLOCKED, 1);
+    }).not.toThrow();
+  });
+
+  it('requires both dimension and value args', () => {
+    expect(() => {
+      new Metric('one-arg');
+    }).toThrow(/Metric constructor needs both a dimension and value for it/);
+    //TODO https://github.com/lfilho/ddg-test-project/issues/46
+  });
+});
