d2985abf1fea0973ff8861e4e8fb0990eb7821d5
FutureDuck <iwonder@duckduckgo.com>
2020-06-14 12:25:42 -0700

    feat: add Logger util

--- src/lib/request_blocker.js
+++ src/lib/request_blocker.js
@@ -1,4 +1,5 @@
 import requestListener from './request_listener.js';
+import Logger from '../shared/util/logger.js';
 
 /**
  * @see https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/webRequest/onBeforeRequest
@@ -8,7 +9,7 @@ export default class RequestBlocker {
     const BLOCKING_FLAG = 'blocking';
     const URL_FILTER = { urls: ['<all_urls>'] };
 
-    browser.webRequest.onErrorOccurred.addListener(console.error, URL_FILTER);
+    browser.webRequest.onErrorOccurred.addListener(Logger.error, URL_FILTER);
 
     browser.webRequest.onBeforeRequest.addListener(
       requestListener,
