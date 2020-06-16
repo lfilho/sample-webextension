6af57efedd898b651ee428867f0849d834bffc6e
FutureDuck <iwonder@duckduckgo.com>
2020-06-16 12:22:35 -0700

    refactor: extract constants outside method

--- src/background/request_blocker.js
+++ src/background/request_blocker.js
@@ -1,19 +1,23 @@
-import requestListener from './request_listener.js';
-import Logger from '../shared/util/logger.js';
-
 /**
  * @see https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/webRequest/onBeforeRequest
  */
+
+import requestListener from './request_listener.js';
+import Logger from '../shared/util/logger.js';
+
+const BLOCKING_FLAG = 'blocking';
+const ALL_URLS_FILTER = { urls: ['<all_urls>'] };
+
 export default class RequestBlocker {
   static startMonitoring() {
-    const BLOCKING_FLAG = 'blocking';
-    const URL_FILTER = { urls: ['<all_urls>'] };
-
-    browser.webRequest.onErrorOccurred.addListener(Logger.error, URL_FILTER);
+    browser.webRequest.onErrorOccurred.addListener(
+      Logger.error,
+      ALL_URLS_FILTER
+    );
 
     browser.webRequest.onBeforeRequest.addListener(
       requestListener,
-      URL_FILTER,
+      ALL_URLS_FILTER,
       [BLOCKING_FLAG]
     );
   }
