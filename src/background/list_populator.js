f109ceec556e7d3ad729da2b46af90787e6c70a8
FutureDuck <iwonder@duckduckgo.com>
2020-06-16 12:05:05 -0700

    refactor: extract constant map to outside method

--- src/background/list_populator.js
+++ src/background/list_populator.js
@@ -7,13 +7,14 @@ import { BAD_URLS, GOOD_URLS } from '../shared/__url_fixtures.js';
 // https://github.com/lfilho/ddg-test-project/issues/18
 // https://github.com/lfilho/ddg-test-project/issues/17
 
+const LIST_TYPE_TO_URL_MAPPER = {
+  [List.types.DENY_LIST]: BAD_URLS,
+  [List.types.ALLOW_LIST]: GOOD_URLS,
+};
+
 export default class ListPopulator {
   static async populateList(list) {
-    const listTypeToUrlMapper = {
-      [List.types.DENY_LIST]: BAD_URLS,
-      [List.types.ALLOW_LIST]: GOOD_URLS,
-    };
-    const urls = listTypeToUrlMapper[list.type];
+    const urls = LIST_TYPE_TO_URL_MAPPER[list.type];
     list.bulkAdd(urls);
   }
 }
